1e1f837515071ec2ee5efe8843db1f1cee7286b3
SuviGaming <60156403+SuviGaming@users.noreply.github.com>
2020-03-02 19:41:43 -0500

    Add files via upload

---
+++ comicbooks/_includes/_utilities/.env.php
@@ -0,0 +1,9 @@
+<?php
+
+  // Our database credentials
+  putenv("DB_HOST=comp-1006.cq2sofwg3vlf.us-east-1.rds.amazonaws.com");
+  putenv("DB_USER=user_midterm");
+  putenv("DB_PASS=comp1006w20midterm");
+  putenv("DB=midterm");
+
+?>
